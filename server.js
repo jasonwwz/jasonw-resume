796f9e5fab9aa42939c203c9c27b8d5aa3dd889a
JASON <jasonwwz@gmail.com>
2019-04-02 13:19:22 -0400

    updated server

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
+const path = require('path');
+app.get('/*all', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
